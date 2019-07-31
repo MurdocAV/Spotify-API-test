e6ed167e71a5ebd4f502ebcad6c95f5ebd036af2
MurdocAV <azaan.virk1@gmail.com>
2019-07-31 16:18:44 +1200

    adjusting spotify api

--- client/api/spotify.js
+++ client/api/spotify.js
@@ -1,20 +1,13 @@
 import request from 'superagent'
 
-const url = "https://api.spotify.com/v1/artists/{id}/related-artists"
+const url = "https://api.spotify.com/v1/albums/{id}/tracks"
 const id = "43ZHCT0cAZBISjO8DG9PnE"
 const token = "BQDcZjvhLRPU0PVGBS6AF2eYAYfQOCiEvxs-bxLbODFJGJNEjSFhPwkbm0hLMophpf0YmUu2CIEUwUZMZolnFC42ncUGOfCw808_UMu1nGrDL2XmZDRVPL7a2rpul6z2y94PHx0pXJmwf-XvDg"
+// Get an album's tracks
 
-
-export function getArtists () {
+export function getAlbumTracks () {
   return request
     .get(url)
     .then(res => res.body)
     .catch(err => console.log(err))
 }
-
-
-
-curl -X "GET" "https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/related-artists" 
--H "Accept: application/json" 
--H "Content-Type: application/json" 
--H "Authorization: Bearer BQDcZjvhLRPU0PVGBS6AF2eYAYfQOCiEvxs-bxLbODFJGJNEjSFhPwkbm0hLMophpf0YmUu2CIEUwUZMZolnFC42ncUGOfCw808_UMu1nGrDL2XmZDRVPL7a2rpul6z2y94PHx0pXJmwf-XvDg"
